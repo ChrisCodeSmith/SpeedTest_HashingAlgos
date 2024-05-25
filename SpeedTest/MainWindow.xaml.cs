ad9de4f4c1212a0bb2de54a7b1a7e2e370d2e12a
ChrisCodeSmith <67827118+ChrisCodeSmith@users.noreply.github.com>
2024-05-25 14:40:24 +0200

    make textboxes readonly and some fixes

--- SpeedTest/MainWindow.xaml.cs
+++ SpeedTest/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.IO;
 using System.Diagnostics;
 using System.Security.Cryptography;
 using System.Runtime.Intrinsics.Arm;
+using System.Runtime.CompilerServices;
+using System.Threading;
 
 namespace SpeedTest
 {
@@ -23,6 +25,8 @@ namespace SpeedTest
         private readonly string path = System.IO.Path.GetTempPath() + "\\FileComp";
 
         private List<Guid> fileList = new List<Guid>();
+
+        private readonly object syncObj = new object();
 
 
 
@@ -80,53 +84,90 @@ namespace SpeedTest
 
         private void RunButton(object sender, RoutedEventArgs e)
         {
-            string[] origFiles = Directory.GetFiles(path);
-            uint md5Collsisions = 0;
-            uint sha256Collisions = 0;
-            StatusBar.Content = "Make copies of files";
-            StatusBar.Foreground = Brushes.Black;
-            foreach (string file in origFiles) { 
-                File.Copy(file, file + ".copy", true);
-            }
-            //Debug.WriteLine(Directory.GetFiles(path).Length);
-
-            // MD5
-            StatusBar.Content = "Start MD5 Calc";
-            StatusBar.Foreground = Brushes.Black;
-            var timer = new Stopwatch();
-            timer.Start();
-            foreach (string origFile in origFiles) {
-                if (!MD5Calc(origFile, origFile + ".copy"))
-                {
-                    StatusBar.Content = "Hash missmatch";
-                    return;
-                }
-                else md5Collsisions++;
-            }
-            timer.Stop();
-            TimeMD5.Text = timer.Elapsed.ToString(@"m\:ss\.fff");
-            CollisionsMD5.Text = md5Collsisions.ToString();
-
-            // SHA256
-            StatusBar.Content = "Start SHA256 Calc";
-            StatusBar.Foreground = Brushes.Black;
-            timer.Restart();
-            foreach (string origFile in origFiles)
+            lock (syncObj)
             {
-                if(!SHA256Calc(origFile, origFile + ".copy"))
+                string[] origFiles = Directory.GetFiles(path);
+                uint markusCollisions = 0;
+                uint md5Collsisions = 0;
+                uint sha256Collisions = 0;
+                StatusBar.Content = "Make copies of files";
+                StatusBar.Foreground = Brushes.Black;
+                foreach (string file in origFiles)
                 {
-                    StatusBar.Content = "Hash missmatch";
-                    StatusBar.Foreground = Brushes.Red;
-                    return;
+                    File.Copy(file, file + ".copy", true);
+                    // Uncomment following 3 lines to test if the comparisons generate collisions if files are different
+                    //StreamWriter sw = File.AppendText(file);
+                    //sw.WriteLine("ffs");
+                    //sw.Close();
                 }
-                else sha256Collisions++;
-            }
-            timer.Stop();
-            TimeSHA256.Text = timer.Elapsed.ToString(@"m\:ss\.fff");
-            CollisionsSHA.Text = sha256Collisions.ToString();
 
-            StatusBar.Content = "All Hash comparisons successfull";
-            StatusBar.Foreground = Brushes.Green;
+                // -----------------------------------------------------------------------
+                // SHA256
+                StatusBar.Content = "Start SHA256 Calc";
+                StatusBar.Foreground = Brushes.Black;
+                var timer = Stopwatch.StartNew();
+                
+                foreach (string origFile in origFiles)
+                {
+                    if (!SHA256Calc(origFile, origFile + ".copy"))
+                    {
+                        StatusBar.Content = "SHA256 Hash missmatch";
+                        StatusBar.Foreground = Brushes.Red;
+                        //return;
+                    }
+                    else sha256Collisions++;
+                }
+                timer.Stop();
+                TimeSHA256.Text = timer.Elapsed.ToString(@"m\:ss\.fff");
+                CollisionsSHA.Text = sha256Collisions.ToString();
+
+                // -----------------------------------------------------------------------
+                // MD5
+                StatusBar.Content = "Start MD5 Calc";
+                StatusBar.Foreground = Brushes.Black;
+                timer.Restart();
+                foreach (string origFile in origFiles)
+                {
+                    if (!MD5Calc(origFile, origFile + ".copy"))
+                    {
+                        StatusBar.Content = "MD5 Hash missmatch";
+                        StatusBar.Foreground = Brushes.Red;
+                        //return;
+                    }
+                    else md5Collsisions++;
+                }
+                timer.Stop();
+                TimeMD5.Text = timer.Elapsed.ToString(@"m\:ss\.fff");
+                CollisionsMD5.Text = md5Collsisions.ToString();
+
+
+                // -----------------------------------------------------------------------
+                // Markus' Algo
+                StatusBar.Content = "Start MD5 Calc";
+                StatusBar.Foreground = Brushes.Black;
+                timer.Restart();
+                foreach (string origFile in origFiles)
+                {
+                    if (!FileCompareMarkus(origFile, origFile + ".copy"))
+                    {
+                        StatusBar.Content = "Markus File compare missmatch or error";
+                        StatusBar.Foreground = Brushes.Red;
+                        //return;
+                    }
+                    else markusCollisions++;
+
+                }
+                timer.Stop();
+                TimeMarkus.Text = timer.Elapsed.ToString(@"m\:ss\.fff");
+                CollisionsMarkus.Text = markusCollisions.ToString();
+
+                
+
+                // -----------------------------------------------------------------------
+
+                StatusBar.Content = "All Hash comparisons successfull";
+                StatusBar.Foreground = Brushes.Green;
+            }
         }
 
         private static bool MD5Calc(string origFile, string copyFile) {
@@ -157,6 +198,50 @@ namespace SpeedTest
                 sBuilder.Append(hashOrig[i].ToString("x2"));
             }
             return sBuilder.ToString();
+        }
+
+        // Markus' FileCompare Method
+
+        // This method accepts two strings the represent two files to
+        // compare. A return value of 0 indicates that the contents of the files
+        // are the same. A return value of any other value indicates that the
+        // files are not the same.
+        private static bool FileCompareMarkus(string file1, string file2)
+        {
+            int file1byte;
+            int file2byte;
+            FileStream fs1;
+            FileStream fs2;
+
+            // Determine if the same file was referenced two times.
+            if (file1 == file2)
+                // Return true to indicate the files are the same.
+                return true;
+
+            // Open the two files.
+            fs1 = new FileStream(file1, FileMode.Open);
+            fs2 = new FileStream(file2 , FileMode.Open);
+            // Check the file sizes. If they are not the same, the files are
+            // not the same.
+            if(fs1.Length != fs2.Length)
+            {
+                // Close the file
+                fs1.Close();
+                fs2.Close();
+                // Return false to indicate files are different
+                return false;
+            }
+            do
+            {
+                file1byte = fs1.ReadByte();
+                file2byte = fs2.ReadByte();
+            }
+            while ((file1byte == file2byte) && (file1byte != -1));
+
+            fs1.Close();
+            fs2.Close();
+
+            return ((file1byte - file2byte) == 0);
         }
     }
 }
